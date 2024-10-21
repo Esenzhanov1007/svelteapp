41cad7d7498ef0ab0a7d6506bbcb7a3129357422
Emil <eesenjanov@gmail.com>
2024-10-22 02:01:09 +0600

    Init project

---
+++ src/components/Checkbox.svelte
@@ -0,0 +1,24 @@
+<script>
+    export let label = "";
+    export let checked = false;
+  </script>
+  
+  <div class="checkbox-group">
+    <input type="checkbox" id="checkbox" bind:checked={checked} />
+    <label for="checkbox">{label}</label>
+  </div>
+  
+  <style>
+    .checkbox-group {
+      display: flex;
+      align-items: center;
+      margin-bottom: 1.5rem;
+    }
+    input[type="checkbox"] {
+      margin-right: 0.5rem;
+    }
+    label {
+      color: white;
+    }
+  </style>
+  
---
+++ src/components/Input.svelte
@@ -0,0 +1,32 @@
+<script>
+  export let placeholder = "";
+  export let required = false;
+  export let value = "";
+</script>
+
+<div class="input-group">
+  <input
+    type="text"
+    class="input"
+    placeholder={placeholder}
+    bind:value
+    required={required}
+  />
+</div>
+
+<style>
+  .input-group {
+    margin-bottom: 1rem;
+  }
+  .input {
+    width: 100%;
+    padding: 0.8rem;
+    border-radius: 5px;
+    border: 1px solid #ccc;
+    background-color: #1e1e2e;
+    color: white;
+  }
+  .input::placeholder {
+    color: #ccc;
+  }
+</style>
